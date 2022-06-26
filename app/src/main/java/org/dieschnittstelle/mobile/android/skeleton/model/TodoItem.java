d1a08f904c137dda3f5a9729f196946764fdcd0b
steve feutat <stevefeutat@gmail.com>
2022-06-26 19:58:45 +0200

    fix on save and update

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/model/TodoItem.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/model/TodoItem.java
@@ -3,14 +3,19 @@ package org.dieschnittstelle.mobile.android.skeleton.model;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 import java.util.Objects;
+
 @Entity
 public class TodoItem implements Serializable {
     @PrimaryKey(autoGenerate = true)
     private long id;
     private String name;
     private String description;
+
+    @SerializedName("done")
     private boolean checked;
 
     public TodoItem() {
@@ -69,11 +74,11 @@ public class TodoItem implements Serializable {
     @Override
     public String toString() {
         return "ToDoItem:{"
-                +"id="+id
-                +",name=" +name
-                +",description=" +description
-                +",checked=" +checked
-                +",super.toString=" +super.toString()
-                +"}";
+                + "id=" + id
+                + ",name=" + name
+                + ",description=" + description
+                + ",checked=" + checked
+                + ",super.toString=" + super.toString()
+                + "}";
     }
 }
