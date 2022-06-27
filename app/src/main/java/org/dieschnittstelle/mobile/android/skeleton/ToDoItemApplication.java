6e20393da589618f54b75b4ca075567afebaef7a
steve feutat <stevefeutat@gmail.com>
2022-06-27 22:50:04 +0200

    add checkConnectivity

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/ToDoItemApplication.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/ToDoItemApplication.java
@@ -1,10 +1,17 @@
 package org.dieschnittstelle.mobile.android.skeleton;
 
 import android.app.Application;
+import android.util.Log;
 
 import org.dieschnittstelle.mobile.android.skeleton.model.IToDoItemCRUDOperations;
 import org.dieschnittstelle.mobile.android.skeleton.model.RetrofitRemoteToDoItemCRUDOperations;
 import org.dieschnittstelle.mobile.android.skeleton.model.RoomLocalToDoItemCRUDOperations;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class ToDoItemApplication extends Application {
     private IToDoItemCRUDOperations crudOperations;
@@ -12,13 +19,43 @@ public class ToDoItemApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        try {
+            if (checkConnectivity().get()) {
+                this.crudOperations = new RetrofitRemoteToDoItemCRUDOperations();
+            } else {
+                this.crudOperations = new RoomLocalToDoItemCRUDOperations(this);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Got exception trying to get future boolean for connectivity");
+        }
         //new RetrofitRemoteToDoItemCRUDOperations();
         // new RoomLocalToDoItemCRUDOperations(this);
         // SimpleToDoItemCRUDOperations.getInstance();
-        this.crudOperations = new RetrofitRemoteToDoItemCRUDOperations();
     }
 
     public IToDoItemCRUDOperations getCrudOperations() {
         return this.crudOperations;
     }
+
+    public Future<Boolean> checkConnectivity() {
+        CompletableFuture<Boolean> result = new CompletableFuture<>();
+        new Thread(() -> {
+            try {
+                HttpURLConnection httpURLConnection = (HttpURLConnection) new URL("http://10.0.2.2:8080/api/todos").openConnection();
+                httpURLConnection.setReadTimeout(1000);
+                httpURLConnection.setConnectTimeout(1000);
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+                httpURLConnection.getInputStream();
+                result.complete(true);
+            } catch (Exception e) {
+                Log.e("IToDoItemCRUDOperations", "Got exception trying to check connectivity: " + e, e);
+                result.complete(false);
+            }
+        }).start();
+
+        return result;
+    }
+
 }
